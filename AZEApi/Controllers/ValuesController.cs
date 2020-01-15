72c33776feaf252bbe81035c0a1e4320b40a9f9c
Tudor Dumitriu <tudor.dumitriu@adaptabi.com>
2020-01-15 11:24:55 +0200

    Build update

--- AZEApi/Controllers/ValuesController.cs
+++ AZEApi/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace AZEApi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "a", "b", "c", "d", "e", "f", "g" };
+            return new string[] { "x" };
         }
 
         // GET api/values/5
